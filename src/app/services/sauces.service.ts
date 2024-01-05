84355858cd035f93e0dbc2cc60d9bdf6aeb2fef6
Hileene <92981899+Hileene@users.noreply.github.com>
2024-01-05 12:21:09 +0100

    Mis à jour Url backend original

--- src/app/services/sauces.service.ts
+++ src/app/services/sauces.service.ts
@@ -3,7 +3,6 @@ import { catchError, mapTo, of, Subject, tap, throwError } from 'rxjs';
 import { Sauce } from '../models/Sauce.model';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth.service';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -15,28 +14,9 @@ export class SaucesService {
   constructor(private http: HttpClient,
               private auth: AuthService) {}
 
-  private updateImageUrls(sauces: Sauce[]): Sauce[] {
-    return sauces.map(sauce => {
-      if (sauce.imageUrl && sauce.imageUrl.startsWith('http://')) {
-        sauce.imageUrl = sauce.imageUrl.replace(/^http:/, 'https:');
-      }
-      return sauce;
-    });
-  }
-
-  private updateImageUrl(sauce: Sauce): Sauce {
-    if (sauce.imageUrl && sauce.imageUrl.startsWith('http://')) {
-      sauce.imageUrl = sauce.imageUrl.replace(/^http:/, 'https:');
-    }
-    return sauce;
-  }
-
   getSauces() {
-    this.http.get<Sauce[]>(`${environment.apiUrl}/sauces`).pipe(
-      tap(sauces => {
-        const saucesWithUpdatedUrls = this.updateImageUrls(sauces);
-        this.sauces$.next(saucesWithUpdatedUrls);
-      }),
+    this.http.get<Sauce[]>('http://localhost:3000/api/sauces').pipe(
+      tap(sauces => this.sauces$.next(sauces)),
       catchError(error => {
         console.error(error.error.message);
         return of([]);
@@ -45,18 +25,14 @@ export class SaucesService {
   }
 
   getSauceById(id: string) {
-    return this.http.get<Sauce>(`${environment.apiUrl}/sauces/${id}`).pipe(
-      tap(sauce => {
-        const sauceWithUpdatedUrl = this.updateImageUrl(sauce);
-        return sauceWithUpdatedUrl;
-      }),
+    return this.http.get<Sauce>('http://localhost:3000/api/sauces/' + id).pipe(
       catchError(error => throwError(error.error.message))
     );
   }
 
   likeSauce(id: string, like: boolean) {
     return this.http.post<{ message: string }>(
-      `${environment.apiUrl}/sauces/${id}/like`,
+      'http://localhost:3000/api/sauces/' + id + '/like',
       { userId: this.auth.getUserId(), like: like ? 1 : 0 }
     ).pipe(
       mapTo(like),
@@ -66,7 +42,7 @@ export class SaucesService {
 
   dislikeSauce(id: string, dislike: boolean) {
     return this.http.post<{ message: string }>(
-      `${environment.apiUrl}/sauces/${id}/like`,
+      'http://localhost:3000/api/sauces/' + id + '/like',
       { userId: this.auth.getUserId(), like: dislike ? -1 : 0 }
     ).pipe(
       mapTo(dislike),
@@ -78,43 +54,28 @@ export class SaucesService {
     const formData = new FormData();
     formData.append('sauce', JSON.stringify(sauce));
     formData.append('image', image);
-    return this.http.post<{ message: string }>(`${environment.apiUrl}/sauces`, formData).pipe(
-      tap(() => {
-        sauce = this.updateImageUrl(sauce);
-        const saucesWithUpdatedUrls = this.updateImageUrls([sauce]);
-        this.sauces$.next(saucesWithUpdatedUrls);
-      }),
+    return this.http.post<{ message: string }>('http://localhost:3000/api/sauces', formData).pipe(
       catchError(error => throwError(error.error.message))
     );
   }
 
   modifySauce(id: string, sauce: Sauce, image: string | File) {
     if (typeof image === 'string') {
-      return this.http.put<{ message: string }>(`${environment.apiUrl}/sauces/${id}`, sauce).pipe(
-        tap(() => {
-          sauce = this.updateImageUrl(sauce);
-          const saucesWithUpdatedUrls = this.updateImageUrls([sauce]);
-          this.sauces$.next(saucesWithUpdatedUrls);
-        }),
+      return this.http.put<{ message: string }>('http://localhost:3000/api/sauces/' + id, sauce).pipe(
         catchError(error => throwError(error.error.message))
       );
     } else {
       const formData = new FormData();
       formData.append('sauce', JSON.stringify(sauce));
       formData.append('image', image);
-      return this.http.put<{ message: string }>(`${environment.apiUrl}/sauces/${id}`, formData).pipe(
-        tap(() => {
-          sauce = this.updateImageUrl(sauce);
-          const saucesWithUpdatedUrls = this.updateImageUrls([sauce]);
-          this.sauces$.next(saucesWithUpdatedUrls);
-        }),
+      return this.http.put<{ message: string }>('http://localhost:3000/api/sauces/' + id, formData).pipe(
         catchError(error => throwError(error.error.message))
       );
     }
   }
 
   deleteSauce(id: string) {
-    return this.http.delete<{ message: string }>(`${environment.apiUrl}/sauces/${id}`).pipe(
+    return this.http.delete<{ message: string }>('http://localhost:3000/api/sauces/' + id).pipe(
       catchError(error => throwError(error.error.message))
     );
   }
